f867d763564f71d0fd805400c1e30c6c530f0ef1
= <=>
2016-03-01 20:09:20 -0800

    Finish and verify ex 10 - time server

---
+++ node/ex10_-_time_server.js
@@ -0,0 +1,19 @@
+var net = require('net');
+
+var port = process.argv[2];
+var date = new Date();
+var year = date.getFullYear().toString();
+var month = (date.getMonth()+1).toString();
+var day = date.getDate().toString();
+var hour = date.getHours().toString();
+var minute = date.getMinutes().toString();
+
+month = (month.length > 1 ? '' : '0')+month;
+day = (day.length > 1 ? '' : '0')+day;
+hour = (hour.length > 1 ? '' : '0')+hour;
+minute = (minute.length > 1 ? '' : '0')+minute;
+
+var server = net.createServer(function(socket){
+  socket.end(year+'-'+month+'-'+day+' '+hour+':'+minute+"\n");
+});
+server.listen(port);
